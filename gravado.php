7cb1dba9bd9aeba1e9597231d1ac5d229e2db4e5
Sara Longo <sara.290607@bento.local>
2023-10-05 16:41:05 -0300

    second commit

---
+++ gravado.php
@@ -0,0 +1,22 @@
+<?php 
+
+    $recados = [];
+
+    // se o arquivo existir...
+    if(file_exists('recados.txt')){
+        $arq_recados = file_get_contents('recados.txt');
+        $recados = unserialize($arq_recados);
+    }
+
+    $recados[] = [
+        'nome' => $_POST['nome'],
+        'data' => date('d/m/Y H:i:s'),
+        'recado' => $_POST['mensagem']
+    ];
+
+    // guarda um novo recado
+    file_put_contents('recados.txt', serialize($recados));
+
+    print_r($recados);
+
+    header('location: index.html');
--- index.php
+++
@@ -1,8 +0,0 @@
-<?php   
-    $nome = $_POST['nome'];
-    $mensagem = $_POST['mensagem'];
-    $data = date('d/m/Y H:i');
-
-    echo "$data";
-    echo "$nome";
-    echo "$mensagem";
